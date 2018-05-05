b2b3f05606670fc77b398ce962233c5fd11b1915
lky9464 <lky9464@gmail.com>
2018-05-05 15:47:18 +0900

    commit at 2018/05/05

---
+++ 1652/1652.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int main() {
+
+	int wallLength;
+	int colLay = 0, rowLay = 0;
+	printf("정사각형 방 가로or세로 길이 입력 : ");
+	scanf("%d", &wallLength);
+
+	//방
+	char **room;
+	room = (char**)malloc(sizeof(char*)*wallLength);
+	for (int i = 0; i < wallLength; i++) {
+		room[i] = (char*)malloc(sizeof(char)*wallLength);
+	}
+	for (int i = 0; i < wallLength; i++) {
+		printf("%d행 방모양(X or .) :", i + 1);
+		scanf("%s", room[i]);
+	}
+	
+	printf("\n그림\n");
+	for (int i = 0; i < wallLength; i++) {
+		for (int j = 0; j < wallLength; j++) {
+			if (room[i][j] == 'X')
+				printf("■");
+			else
+				printf("□");
+		}
+		printf("\n");
+	}
+
+	//if wallLength == 5
+
+
+	//가로로 눕기
+
+	for (int i = 1; i < wallLength - 1; i++) {
+		for (int j = 0; j < wallLength; j++) {
+
+			
+			if (room[j][i] == '.') {
+				if (room[j][i + 1] == '.' && room[j][i - 1] == '.')
+					colLay += 1;
+			}
+		}
+	}
+	//세로
+	for (int i = 1; i < wallLength - 1; i++) {
+		for (int j = 0; j < wallLength; j++) {
+
+			if (room[i][j] == '.') {
+				if (room[i+1][j] == '.' && room[i-1][j] == '.')
+					rowLay += 1;
+			}
+
+
+
+		}
+	}
+
+	printf("가로로 누울 수 있는 자리 >> %d자리\n", colLay);
+	printf("세로로 누울 수 있는 자리 >> %d자리\n", rowLay);
+
+
+
+
+
+
+
+	//해제
+	free(room);
+	
+
+	return 0;
+}
